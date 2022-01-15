fafea33ee5cad6d716e37a30d969e4c9a3d5b873
Karl Voit <git@Karl-Voit.at>
2022-01-15 23:49:55 +0100

    draft of extended pattern version with first tool functions

--- filetagslib/filetagslib.py
+++ filetagslib/filetagslib.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8; mode: python; -*-
-# Time-stamp: <2019-12-14 11:41:58 vk>
+# Time-stamp: <2022-01-15 23:49:33 vk>
 
-import logging
-import re
+import logging # let's log stuff helpful for the user and for debugging
+import re      # oh, this is using regular expressions quite a bit :-)
+from typing import List, Union, Tuple, Optional  # mypy: type checks
+
+# for handling date and time:
+import locale
+import datetime
+locale.setlocale(locale.LC_ALL, '')
+
 
 
 class filenameconvention():
@@ -11,16 +18,161 @@ class filenameconvention():
     Utility library related to filenameconvention of filetags.
     """
 
-    BETWEEN_TAG_SEPARATOR = ' '
-    FILENAME_TAG_SEPARATOR = ' -- '
-    DATESTAMP_PATTERN = '(?P<datestamp>(?P<year>\d{4,4})-(?P<month>[01]\d)-(?P<day>[0123]\d))'
-    HMS_PATTERN = '(?P<hours>[012]\d)\.(?P<minutes>[012345]\d)(\.(?P<seconds>[012345]\d))?'
-    TIMESTAMP_PATTERN = DATESTAMP_PATTERN + '(T(?P<timestamp>' + HMS_PATTERN + '))?'
+    #DATESTAMP_PATTERN = '(?P<datestamp>(?P<year>\d{4,4})-(?P<month>[01]\d)-(?P<day>[0123]\d))'
+    #HMS_PATTERN = '(?P<hours>[012]\d)\.(?P<minutes>[012345]\d)(\.(?P<seconds>[012345]\d))?'
+    #TIMESTAMP_PATTERN = DATESTAMP_PATTERN + '(T(?P<timestamp>' + HMS_PATTERN + '))?'
+    #FILENAME_PATTERN_REGEX = re.compile('^(?P<datetimestamp>' + TIMESTAMP_PATTERN + ')' +
+    #                                    '([-_ ](?P<description>.+?))??' +
+    #                                    '(' + FILETAGS_PATTERN + ')?' +
+    #                                    '\.(?P<extension>\w+)$')
+
+    YMD_SEPARATORS = '[-_.]'         # potential separator character between the entities of year, month, day
+    DATETIME_SEPARATORS = '[T: -_]'  # potential separator character between the entities of datestamp and timestamp
+    HMS_SEPARATORS = '[:.-]'         # potential separator character between the entities of hour, minute, second
+    END_SEPARATORS = '[^a-zA-Z0-9]'  # potential separator character between the entities of datetimestamp and rest
+    BETWEEN_TAG_SEPARATOR = ' '      # mandatory separator between two different tags
+    FILENAME_TAG_SEPARATOR = ' -- '  # mandatory separator between normal file name and list of tags (if there are tags)
     FILETAGS_PATTERN = FILENAME_TAG_SEPARATOR.rstrip() + '([ ](?P<filetags>.+))+'
-    FILENAME_PATTERN_REGEX = re.compile('^(?P<datetimestamp>' + TIMESTAMP_PATTERN + ')' +
-                                        '([-_ ](?P<description>.+?))??' +
-                                        '(' + FILETAGS_PATTERN + ')?' +
-                                        '\.(?P<extension>\w+)$')
+
+    # The regular expression matches optional date- and time-stamps as long as order is YMDHM(S).
+    # Simplified:   (YY)?YY.MM.DD.HH.MM(.SS)?
+    # Frequently used examples: (more combinations are matching)
+    #    YYYY-MM-DDTHH.MM.SS foo.bar
+    #    YYYY-MM-DDTHH.MM foo.bar
+    #    YYYY-MM-DD foo.bar
+    #    YYYYMMDDTHH.MM.SS_foo.bar
+    #    YYYYMMDDTHHMMSS_foo.bar
+    #    YYYY_MM_DDTHH:MM:SS_foo.bar
+    #
+    # For other orders like MM/DD/YYYY: please do re-think your life choices. ;-)  *SCNR*
+    # Unsupported:
+    #    - non-ISO-like orders of the entities
+    #    - time zones or time offsets
+    #    - weeks
+    #    - durations or intervals
+    #    - milliseconds
+    #
+    # The separation characters are limited to sets of potential characters (see regex for details).
+    FILENAME_PATTERN_REGEX = \
+        re.compile(r'^' +
+                   r'(?P<datetimestamp>' +                              # BEGIN: datetimestamp: datetimestamp with separator (OPTIONAL)
+                   r'(?P<datestamp>' +                                  #   BEGIN: datestamp
+                   r'(?P<century>\d{2})?' +                             #     optional century: YY    e.g. 20 (from 2022)
+                   r'(?P<year>\d{2})' +                                 #     YY    e.g. 22 (from 2022)
+                   r'(?P<ym_sep>' + YMD_SEPARATORS + ')?' +             #     optional separator character
+                   r'(?P<month>[01]\d)' +                               #     MM    e.g. 12 (December)
+                   r'(?P<md_sep>' + YMD_SEPARATORS + ')?' +             #     optional separator character
+                   r'(?P<day>[0123]\d)' +                               #     DD    e.g. 31
+                   r')?' +                                              #   END: datestamp
+                   r'(' +                                               #   BEGIN: timestamppart is optional
+                   r'(?P<datetime_sep>' + DATETIME_SEPARATORS + ')?' +  #     optional separator character
+                   r'(?P<timestamp>' +                                  #     BEGIN: timestamp
+                   r'(?P<hour>[012]\d)' +                               #       HH    e.g. 23
+                   r'(?P<hm_sep>' + HMS_SEPARATORS + ')?' +             #       optional separator character
+                   r'(?P<minute>[012345]\d)' +                          #       MM    e.g. 59
+                   r'(' +                                               #       BEGIN: seconds are optional
+                   r'(?P<ms_sep>' + HMS_SEPARATORS + ')?' +             #         optional separator character
+                   r'(?P<second>[012345]\d)' +                          #         SS    e.g. 59
+                   r')?' +                                              #       END: seconds are optional
+                   r')?' +                                              #     END: timestamp
+                   r')?' +                                              #   END: timestamppart is optional
+                   r')?' +                                              # END: datetimestamp: datetimestamp
+                   r'(?P<end_sep>' + END_SEPARATORS + ')' +             # mandatory separator character
+                   r'(?P<description>.+?)??' +                          # the usual file name which is not date/timestamp prefix, filetags or extension
+                   r'(' + FILETAGS_PATTERN + ')?' +                     # the optional list of filetags
+                   r'\.(?P<extension>\w+)$'                             # the mandatory file extension
+        )
+
+    # examples:
+
+    # re.match(FILENAME_PATTERN_REGEX, '2022-01-14T17.53.16_foo -- bar.baz').groups()
+    #  -> FIXXME
+
+    # re.match(FILENAME_PATTERN_REGEX, '2022-01-14T17.53.16_foo -- bar.baz').groupdict()
+    #  -> FIXXME
+
+    def is_filenamepattern_match(self, mymatch: Union[None, re.Match]) -> bool:
+        return mymatch is not None
+
+    def has_match_century(self, mymatch: re.Match) -> bool:
+        return mymatch.group('century') is not None
+
+    def has_match_year(self, mymatch: re.Match) -> bool:
+        return mymatch.group('year') is not None
+
+    def has_match_month(self, mymatch: re.Match) -> bool:
+        return mymatch.group('month') is not None
+
+    def has_match_day(self, mymatch: re.Match) -> bool:
+        return mymatch.group('day') is not None
+
+    def has_match_datestamp(self, mymatch: re.Match) -> bool:
+        return mymatch.group('datestamp') is not None
+
+    def has_match_hours(self, mymatch: re.Match) -> bool:
+        return mymatch.group('hour') is not None
+
+    def has_match_minutes(self, mymatch: re.Match) -> bool:
+        return mymatch.group('minute') is not None
+
+    def has_match_seconds(self, mymatch: re.Match) -> bool:
+        return mymatch.group('second') is not None
+
+    def has_match_timestamp(self, mymatch: re.Match) -> bool:
+        return mymatch.group('timestamp') is not None
+
+    def has_match_description(self, mymatch: re.Match) -> bool:
+        return mymatch.group('description') is not None
+
+    def has_match_filetags(self, mymatch: re.Match) -> bool:
+        return mymatch.group('filetags') is not None
+
+    def has_match_extension(self, mymatch: re.Match) -> bool:
+        return mymatch.group('extension') is not None
+
+    def get_match_full_year(self, mymatch: re.Match) -> str:
+        if not self.has_match_century(mymatch):
+            return '20' + mymatch.group('year')  # this defaults to the 21th century
+        else:
+            return mymatch.group('century') + mymatch.group('year')
+
+    def get_datetime(self, mymatch: re.Match) -> datetime.datetime:
+
+        assert self.is_filenamepattern_match(mymatch)
+        year = int(self.get_match_full_year(mymatch))
+
+        if self.has_match_timestamp(mymatch):
+            if self.has_match_seconds(mymatch):
+                return datetime.datetime(year, int(mymatch.group('month')), int(mymatch.group('day')), int(mymatch.group('hour')), int(mymatch.group('minute')), int(mymatch.group('second')))
+            else:
+                return datetime.datetime(year, int(mymatch.group('month')), int(mymatch.group('day')), int(mymatch.group('hour')), int(mymatch.group('minute')), 0)
+        else:
+            return datetime.datetime(year, int(mymatch.group('month')), int(mymatch.group('day')) )
+
+    def format_datetime_string_according_to_match(self, mydatetime: datetime.datetime, mymatch: re.Match) -> str:
+
+        assert self.is_filenamepattern_match(mymatch)
+        if self.has_match_century(mymatch):
+            year = str(mydatetime.year)
+        else:
+            year = str(mydatetime.year)[2:]
+
+        # separators: ['hm_sep', 'end_sep', 'md_sep', 'ms_sep', 'datetime_sep', 'ym_sep']
+
+        if self.has_match_timestamp(mymatch):
+            if self.has_match_seconds(mymatch):
+                return year + mymatch.group('ym_sep') + str(mydatetime.month) + mymatch.group('md_sep') + str(mydatetime.day) + \
+                    mymatch.group('datetime_sep') + str(mydatetime.hour) + mymatch.group('hm_sep') + \
+                    str(mydatetime.minute) + mymatch.group('ms_sep') + str(mydatetime.second) + mymatch.group('end_sep')
+            else:
+                return year + mymatch.group('ym_sep') + str(mydatetime.month) + mymatch.group('md_sep') + \
+                    str(mydatetime.day) + mymatch.group('datetime_sep') + str(mydatetime.hour) + \
+                    mymatch.group('hm_sep') + str(mydatetime.minute) + mymatch.group('end_sep')
+        else:
+            return year + mymatch.group('ym_sep') + str(mydatetime.month) + mymatch.group('md_sep') + \
+                    str(mydatetime.day) + mymatch.group('end_sep')
+
+
 
 # Local Variables:
 # End:
--- filetagslib/filetagslib_test.py
+++ filetagslib/filetagslib_test.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2019-12-14 11:43:04 vk>
+# Time-stamp: <2022-01-15 23:47:08 vk>
 
 import unittest
 import re
+import datetime
 from filetagslib.filetagslib import filenameconvention
 
 
@@ -12,7 +13,7 @@ class TestOrgFormat(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_filename_pattern_regex_with_timestamp_seconds_description_tags(self):
+    def test_filename_pattern_regex_with_timestamp_second_description_tags(self):
 
         self.assertEqual(
             re.match(filenameconvention.FILENAME_PATTERN_REGEX, '2019-12-13T18.01.23 foo bar -- baz1 baz2 baz3.txt').groups(),
@@ -43,14 +44,14 @@ class TestOrgFormat(unittest.TestCase):
         self.assertEqual(components.group('month'), '12')
         self.assertEqual(components.group('day'), '13')
         self.assertEqual(components.group('timestamp'), '18.01.23')
-        self.assertEqual(components.group('hours'), '18')
-        self.assertEqual(components.group('minutes'), '01')
-        self.assertEqual(components.group('seconds'), '23')
+        self.assertEqual(components.group('hour'), '18')
+        self.assertEqual(components.group('minute'), '01')
+        self.assertEqual(components.group('second'), '23')
         self.assertEqual(components.group('description'), 'foo bar')
         self.assertEqual(components.group('filetags'), 'baz1 baz2 baz3')
         self.assertEqual(components.group('extension'), 'txt')
 
-    def test_filename_pattern_regex_with_timestamp_noseconds_description_tags(self):
+    def test_filename_pattern_regex_with_timestamp_nosecond_description_tags(self):
 
         components = re.match(filenameconvention.FILENAME_PATTERN_REGEX, '2019-12-13T18.01 foo bar -- baz1 baz2 baz3.txt')
         self.assertEqual(components.group('datestamp'), '2019-12-13')
@@ -59,9 +60,9 @@ class TestOrgFormat(unittest.TestCase):
         self.assertEqual(components.group('month'), '12')
         self.assertEqual(components.group('day'), '13')
         self.assertEqual(components.group('timestamp'), '18.01')
-        self.assertEqual(components.group('hours'), '18')
-        self.assertEqual(components.group('minutes'), '01')
-        self.assertEqual(components.group('seconds'), None)
+        self.assertEqual(components.group('hour'), '18')
+        self.assertEqual(components.group('minute'), '01')
+        self.assertEqual(components.group('second'), None)
         self.assertEqual(components.group('description'), 'foo bar')
         self.assertEqual(components.group('filetags'), 'baz1 baz2 baz3')
         self.assertEqual(components.group('extension'), 'txt')
@@ -75,14 +76,14 @@ class TestOrgFormat(unittest.TestCase):
         self.assertEqual(components.group('month'), '12')
         self.assertEqual(components.group('day'), '13')
         self.assertEqual(components.group('timestamp'), None)
-        self.assertEqual(components.group('hours'), None)
-        self.assertEqual(components.group('minutes'), None)
-        self.assertEqual(components.group('seconds'), None)
+        self.assertEqual(components.group('hour'), None)
+        self.assertEqual(components.group('minute'), None)
+        self.assertEqual(components.group('second'), None)
         self.assertEqual(components.group('description'), 'foo bar')
         self.assertEqual(components.group('filetags'), 'baz1 baz2 baz3')
         self.assertEqual(components.group('extension'), 'txt')
 
-    def test_filename_pattern_regex_with_timestamp_seconds_nodescription_tags(self):
+    def test_filename_pattern_regex_with_timestamp_second_nodescription_tags(self):
 
         components = re.match(filenameconvention.FILENAME_PATTERN_REGEX, '2019-12-13T18.01.23 -- baz1 baz2 baz3.txt')
         self.assertEqual(components.group('datestamp'), '2019-12-13')
@@ -91,30 +92,37 @@ class TestOrgFormat(unittest.TestCase):
         self.assertEqual(components.group('month'), '12')
         self.assertEqual(components.group('day'), '13')
         self.assertEqual(components.group('timestamp'), '18.01.23')
-        self.assertEqual(components.group('hours'), '18')
-        self.assertEqual(components.group('minutes'), '01')
-        self.assertEqual(components.group('seconds'), '23')
+        self.assertEqual(components.group('hour'), '18')
+        self.assertEqual(components.group('minute'), '01')
+        self.assertEqual(components.group('second'), '23')
         self.assertEqual(components.group('description'), None)
         self.assertEqual(components.group('filetags'), 'baz1 baz2 baz3')
         self.assertEqual(components.group('extension'), 'txt')
 
-    def test_filename_pattern_regex_with_timestamp_seconds_description_notags(self):
+    def test_filename_pattern_regex_with_timestamp_second_description_notags(self):
 
         components = re.match(filenameconvention.FILENAME_PATTERN_REGEX, '2019-12-13T18.01.23 foo bar.txt')
-        self.assertEqual(components.group('datestamp'), '2019-12-13')
         self.assertEqual(components.group('datetimestamp'), '2019-12-13T18.01.23')
-        self.assertEqual(components.group('year'), '2019')
+        self.assertEqual(components.group('datestamp'), '2019-12-13')
+        self.assertEqual(components.group('century'), '20')
+        self.assertEqual(components.group('year'), '19')
+        self.assertEqual(components.group('ym_sep'), '-')
         self.assertEqual(components.group('month'), '12')
+        self.assertEqual(components.group('md_sep'), '-')
         self.assertEqual(components.group('day'), '13')
+        self.assertEqual(components.group('datetime_sep'), 'T')
         self.assertEqual(components.group('timestamp'), '18.01.23')
-        self.assertEqual(components.group('hours'), '18')
-        self.assertEqual(components.group('minutes'), '01')
-        self.assertEqual(components.group('seconds'), '23')
+        self.assertEqual(components.group('hour'), '18')
+        self.assertEqual(components.group('hm_sep'), '.')
+        self.assertEqual(components.group('minute'), '01')
+        self.assertEqual(components.group('ms_sep'), '.')
+        self.assertEqual(components.group('second'), '23')
+        self.assertEqual(components.group('end_sep'), ' ')
         self.assertEqual(components.group('description'), 'foo bar')
         self.assertEqual(components.group('filetags'), None)
         self.assertEqual(components.group('extension'), 'txt')
 
-    def test_filename_pattern_regex_with_timestamp_seconds_nodescription_notags(self):
+    def test_filename_pattern_regex_with_timestamp_second_nodescription_notags(self):
 
         components = re.match(filenameconvention.FILENAME_PATTERN_REGEX, '2019-12-13T18.01.23.txt')
         self.assertEqual(components.group('datestamp'), '2019-12-13')
@@ -123,14 +131,14 @@ class TestOrgFormat(unittest.TestCase):
         self.assertEqual(components.group('month'), '12')
         self.assertEqual(components.group('day'), '13')
         self.assertEqual(components.group('timestamp'), '18.01.23')
-        self.assertEqual(components.group('hours'), '18')
-        self.assertEqual(components.group('minutes'), '01')
-        self.assertEqual(components.group('seconds'), '23')
+        self.assertEqual(components.group('hour'), '18')
+        self.assertEqual(components.group('minute'), '01')
+        self.assertEqual(components.group('second'), '23')
         self.assertEqual(components.group('description'), None)
         self.assertEqual(components.group('filetags'), None)
         self.assertEqual(components.group('extension'), 'txt')
 
-    def test_filename_pattern_regex_with_timestamp_noseconds_nodescription_notags(self):
+    def test_filename_pattern_regex_with_timestamp_nosecond_nodescription_notags(self):
 
         components = re.match(filenameconvention.FILENAME_PATTERN_REGEX, '2019-12-13T18.01.txt')
         self.assertEqual(components.group('datestamp'), '2019-12-13')
@@ -139,14 +147,14 @@ class TestOrgFormat(unittest.TestCase):
         self.assertEqual(components.group('month'), '12')
         self.assertEqual(components.group('day'), '13')
         self.assertEqual(components.group('timestamp'), '18.01')
-        self.assertEqual(components.group('hours'), '18')
-        self.assertEqual(components.group('minutes'), '01')
-        self.assertEqual(components.group('seconds'), None)
+        self.assertEqual(components.group('hour'), '18')
+        self.assertEqual(components.group('minute'), '01')
+        self.assertEqual(components.group('second'), None)
         self.assertEqual(components.group('description'), None)
         self.assertEqual(components.group('filetags'), None)
         self.assertEqual(components.group('extension'), 'txt')
 
-    def test_filename_pattern_regex_with_datestamp_notimestamp_noseconds_nodescription_notags(self):
+    def test_filename_pattern_regex_with_datestamp_notimestamp_nosecond_nodescription_notags(self):
 
         components = re.match(filenameconvention.FILENAME_PATTERN_REGEX, '2019-12-13.txt')
         self.assertEqual(components.group('datestamp'), '2019-12-13')
@@ -155,12 +163,77 @@ class TestOrgFormat(unittest.TestCase):
         self.assertEqual(components.group('month'), '12')
         self.assertEqual(components.group('day'), '13')
         self.assertEqual(components.group('timestamp'), None)
-        self.assertEqual(components.group('hours'), None)
-        self.assertEqual(components.group('minutes'), None)
-        self.assertEqual(components.group('seconds'), None)
+        self.assertEqual(components.group('hour'), None)
+        self.assertEqual(components.group('minute'), None)
+        self.assertEqual(components.group('second'), None)
         self.assertEqual(components.group('description'), None)
         self.assertEqual(components.group('filetags'), None)
         self.assertEqual(components.group('extension'), 'txt')
+
+    def test_get_datetime(self):
+        # testing various file names including seconds:
+        myfilenames = ['2022-01-14T17:53:16_foo -- bar.baz',
+                       '2022-01-14T17.53.16_foo -- bar.baz',
+                       '2022-01-14T17.53.16 foo -- bar.baz',
+                       '2022_01_14T17.53.16 foo -- bar.baz',
+                       '20220114175316_foo -- bar.baz',
+                       '20220114T175316_foo -- bar.baz',
+                       '20220114T17.53.16_foo -- bar.baz']
+        for myfilename in myfilenames:
+            mymatch = re.match(filenameconvention.FILENAME_PATTERN_REGEX, myfilename)
+            self.assertEqual(filenameconvention.get_datetime(mymatch), datetime.datetime(2022, 1, 14, 17, 53, 16))
+
+        # testing various file names without seconds:
+        myfilenames = ['2022-01-14T17:53_foo -- bar.baz',
+                       '2022-01-14T17.53_foo -- bar.baz',
+                       '2022-01-14T17.53 foo -- bar.baz',
+                       '2022_01_14T17.53 foo -- bar.baz',
+                       '202201141753_foo -- bar.baz',
+                       '20220114T1753_foo -- bar.baz',
+                       '20220114T17.53_foo -- bar.baz']
+        for myfilename in myfilenames:
+            mymatch = re.match(filenameconvention.FILENAME_PATTERN_REGEX, myfilename)
+            self.assertEqual(filenameconvention.get_datetime(mymatch), datetime.datetime(2022, 1, 14, 17, 53))
+
+        # testing various file names without timestamps:
+        myfilenames = ['2022-01-14_foo -- bar.baz',
+                       '2022_01_14_foo -- bar.baz',
+                       '2022_01_14 foo -- bar.baz',
+                       '2022.01.14_foo -- bar.baz']
+        for myfilename in myfilenames:
+            mymatch = re.match(filenameconvention.FILENAME_PATTERN_REGEX, myfilename)
+            self.assertEqual(filenameconvention.get_datetime(mymatch), datetime.datetime(2022, 1, 14, 17, 53))
+
+    def test_format_datetime_string_according_to_match(self):
+
+        # the goal is to take the file name pattern and replace its
+        # date/timestamp with the date/time of mydatetime instead of
+        # the original one:
+        mydatetime = datetime.datetime(2999, 12, 31, 23, 59, 58)
+
+        # the test file names and their replaced counterparts:
+        myfilenames = [['2022-01-14T17:53:16_foo -- bar.baz', '2999-12-31T23:59:58_foo -- bar.baz'],
+                       ['2022-01-14T17.53.16_foo -- bar.baz', '2999-12-31T23.59.58_foo -- bar.baz'],
+                       ['2022-01-14T17.53.16 foo -- bar.baz', '2999-12-31T23.59.58 foo -- bar.baz'],
+                       ['2022_01_14T17.53.16 foo -- bar.baz', '2999_12_31T23.59.58 foo -- bar.baz'],
+                       ['20220114175316_foo -- bar.baz', '29991231235958_foo -- bar.baz'],
+                       ['20220114T175316_foo -- bar.baz', '29991231T235958_foo -- bar.baz'],
+                       ['20220114T17.53.16_foo -- bar.baz', '29991231T23.59.58_foo -- bar.baz'],
+                       ['2022-01-14T17:53_foo -- bar.baz', '2999-12-31T23:59_foo -- bar.baz'],
+                       ['2022-01-14T17.53_foo -- bar.baz', '2999-12-31T23.59_foo -- bar.baz'],
+                       ['2022-01-14T17.53 foo -- bar.baz', '2999-12-31T23.59 foo -- bar.baz'],
+                       ['2022_01_14T17.53 foo -- bar.baz', '2999_12_31T23.59 foo -- bar.baz'],
+                       ['202201141753_foo -- bar.baz', '299912312359_foo -- bar.baz'],
+                       ['20220114T1753_foo -- bar.baz', '29991231T2359_foo -- bar.baz'],
+                       ['20220114T17.53_foo -- bar.baz', '29991231T23.59_foo -- bar.baz'],
+                       ['2022-01-14_foo -- bar.baz', '2999-12-31_foo -- bar.baz'],
+                       ['2022_01_14_foo -- bar.baz', '2999_12_31_foo -- bar.baz'],
+                       ['2022_01_14 foo -- bar.baz', '2999_12_31 foo -- bar.baz'],
+                       ['2022.01.14_foo -- bar.baz', '2999.12.31_foo -- bar.baz']]
+
+        for myfilename in myfilenames:
+            mymatch = re.match(filenameconvention.FILENAME_PATTERN_REGEX, myfilename[0])
+            self.assertEqual(filenameconvention.format_datetime_string_according_to_match(mydatetime, mymatch), myfilename[1])
 
 # Local Variables:
 # End:
